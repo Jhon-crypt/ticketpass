1eb8929e9004c5250516f367be7c8a10893dca4e
Sulaimon Taofeek Ayomide <sulaimontaofeekgithub100@gmail.com>
2023-12-09 22:37:06 +0100

    first commit

--- app/page.js
+++ app/page.js
@@ -1,22 +1,17 @@
 import Link from "next/link"
 import BootstrapClientJs from "./components/bootstrapJsClient/bootstrapClient"
+import HeroPage from "./components/landingPage/hero"
 
 export default function Home() {
   return (
     <>
 
-      <div className="container mt-5">
-        <h1>Home</h1>
-        <p className="lead">
-          <Link href="/signin">Go to sign in</Link>
-        </p>
-        <p className="lead">
-          <Link href="/events">Events</Link>
-        </p>
-        <p className="lead">
-          <Link href="/ticket">Tickets</Link>
-        </p>
-      </div>
+        <div>
+      
+          <HeroPage/> 
+          
+
+        </div>
 
     </>
   )
