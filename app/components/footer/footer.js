45a5436b952781eebe8ab3d8d62cd50de407ca50
Autocredit <wikiwoo@Ashymzs-MacBook-Pro.local>
2023-12-10 19:15:43 +0100

    updates on header and payment page

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
       <div class="container-fluid bg-dark">
        
   <footer class="py-3 my-4">
-    <div className="text-center pb-3 mb-3 text-light">
+    <div className="text-center pt-3 pb-3 mb-3 text-light">
     Questions or enquiries? Send us a message <br></br> You can also call us on +234 XXX XXX XXXX 👋
     </div>
    
