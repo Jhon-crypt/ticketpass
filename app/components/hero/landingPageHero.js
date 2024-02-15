85255ddfd134016a0fc642bfba90a234b3e83edb
Jhon-crypt <johnoladele690@gmail.com>
2024-02-15 11:49:40 +0100

    Added clash display font

--- app/components/hero/landingPageHero.js
+++ app/components/hero/landingPageHero.js
@@ -11,7 +11,7 @@ export default function LandingPageHero() {
 
                         <div className="px-4 my-5 text-center">
                             {/* <img className="d-block mx-auto mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" /> */}
-                            <h1 id="text_header" className="display-5 hero-title fw-bold text-white">Your one-stop shop <br />for your ticket</h1>
+                            <h1 id="text_header" className="hero-title fw-bold text-white">Your one-stop shop for your ticket</h1>
                             <div className="col-lg-9 mb-3 mx-auto">
                                 <p id="text_lead" className="hero-content mb-4 text-white">"Unlock Seamless Access to Unforgettable Experiences: Explore Our Comprehensive Ticket Hub". A ticketing platform for events, cinema with wonderful experiences.</p>
                                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
